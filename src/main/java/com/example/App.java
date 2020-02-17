9c8495285d486fff3593fe2fe6a4481783dd77be
Joker <a.janars@gmail.com>
2020-02-17 23:04:38 +0600

    first commit

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import javax.swing.*;
+
+public class App {
+    private JTextArea textArea1;
+    private JButton button1;
+    private JTextArea textArea2;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
